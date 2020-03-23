dd263086e7f131bff30ce87242c31ef030ea2d57
Baptiste Bertrand-Rapello <baptiste2394@gmail.com>
2020-03-23 16:13:06 +0100

    i progress

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,13 @@
+import { Hero } from './hero'
+
+export const HEROES: Hero[] = [
+  { id: 11, name: 'Adrien' },
+  { id: 12, name: 'Antoine' },
+  { id: 13, name: 'Nathan' },
+  { id: 14, name: 'Ariless' },
+  { id: 15, name: 'Bogdan' },
+  { id: 16, name: 'Romain' },
+  { id: 17, name: 'Zaydan' },
+  { id: 18, name: 'Kenga' },
+  { id: 19, name: 'Baptiste' }
+  ];
